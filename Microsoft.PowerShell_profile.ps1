57e7892d704d8f36541afc5429e252f7f9ccebc1
rachmadandy <rachmadandymahendras@gmail.com>
2022-12-23 14:34:28 +0700

    add cpc function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,6 +13,10 @@ Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -BellStyle Visual
 
+function cpc{
+    Copy-Item 'C:\Users\rachm\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1' 'C:\Users\rachm\.config\'
+}
+
 function cpwd{
     Set-Clipboard -Value $pwd
 }
